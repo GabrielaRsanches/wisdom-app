678d4b16930b2e3b8c4d7a7278a6460b943cfe3e
Gabriela Sanches <gabi@toki.life>
2024-03-05 14:48:34 -0300

    feat: placeholder folder to set basic data fetch structure

---
+++ web/app/shared/placeholder-data.js
@@ -0,0 +1,26 @@
+// This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
+// https://nextjs.org/learn/dashboard-app/fetching-data
+const students = [
+  {
+   
+  },
+];
+
+const teachers = [
+
+];
+
+const questions = [
+ 
+];
+
+const answers = [
+ 
+];
+
+module.exports = {
+  students,
+  teachers,
+  questions,
+  answers,
+};
