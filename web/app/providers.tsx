384eeccb2a9cc3890fb07a976d04758c1fb704ef
Gabriela Sanches <gabi@toki.life>
2024-03-09 17:43:15 -0300

    feat: initial logic to fetchData

--- web/app/providers.tsx
+++ web/app/providers.tsx
@@ -1,7 +1,25 @@
 'use client'
 
 import { ChakraProvider } from '@chakra-ui/react'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { TeacherRegistryHandler, StudentRegistryHandler, QuestionHandler } from './api/data';
 
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>
+export function Providers({ children, includeForm = false }: { children: React.ReactNode,  includeForm?: boolean; }) {
+  const [data, setData] = useState<typeof TeacherRegistryHandler | typeof StudentRegistryHandler | typeof QuestionHandler | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get<typeof TeacherRegistryHandler | typeof StudentRegistryHandler | typeof QuestionHandler >('/api/data'); // Relative path to your API route
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+  
+  return <ChakraProvider> <pre>{JSON.stringify(data, null, 2)}</pre>{ children}</ChakraProvider>
 }
