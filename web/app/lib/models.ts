ffc327202e71596feab5444649c67ab8dcfa6d3e
Gabriela Sanches <gabi@toki.life>
2024-05-01 11:55:21 -0300

    feat: frontend interfaces

---
+++ web/app/lib/models.ts
@@ -0,0 +1,40 @@
+import { TeachingArea, Grade } from '../../../domain/shared/enum'
+import { Email } from '../../../domain/shared/interfaces'; 
+import { Student } from '../../../domain/src/student/Student';
+import FileUpload, { ConfirmedPassword } from '../shared/utils';
+
+
+interface TeacherRegistry {
+  teacherId: string;
+  name: string;
+  email: string;
+  password: string; 
+  confirmPassword: string;
+  teachingAreas: TeachingArea[];
+  credentials: typeof FileUpload;
+  error: {}
+}
+
+interface StudentRegistry {
+  name: string;
+  email: Email;
+  password: string;
+  confirmPassword: ConfirmedPassword;
+  grade: Grade;
+
+  
+}
+
+interface Question {
+  student: Student;
+  title: string;
+  description: string;
+  answer?: Answer[]
+}
+
+interface Answer {
+  question: Question;
+  text: string
+}
+
+export type { TeacherRegistry, StudentRegistry, Question, Answer };
