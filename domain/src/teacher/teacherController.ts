bc6888fc11b6da072f49dc75b77638a4d107b0bc
Gabriela Sanches <gabi@toki.life>
2024-05-03 12:54:06 -0300

    feat: TeacherController and TeacherService implementation

--- domain/src/teacher/teacherController.ts
+++ domain/src/teacher/teacherController.ts
@@ -1,17 +1,31 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, BadRequestException, NotFoundException } from '@nestjs/common';
 import { TeacherService } from './teacherService';
 import { CreateTeacherDto } from './dto/createTeacher';
-import { Teacher } from './Teacher';
+import { Teacher } from './teacher';
+import { TeacherLoginDto } from './dto/loginTeacher';
 
 @Controller('teacher')
 export class TeacherController {
   constructor(private readonly teacherService: TeacherService) {}
 
-  @Post()
+  @Post('register')
   async create(@Body() createTeacherDto: CreateTeacherDto): Promise<Teacher> {
+    const existingTeacher = this.teacherService.findById(createTeacherDto.teacherId);
+    if (existingTeacher) {
+      throw new BadRequestException('Account already exists');
+    }
     return this.teacherService.create(createTeacherDto);
   }
 
+  @Post('login')
+  async login(@Body() loginTeacherDto: TeacherLoginDto): Promise<Teacher> {
+    const teacher = this.teacherService.findByEmailAndPassword(loginTeacherDto.email, loginTeacherDto.password);
+    if (!teacher) {
+      throw new NotFoundException('Teacher not found');
+    }
+    return teacher;
+  }
+  
   @Get()
   async findAll(): Promise<Teacher[]> {
     return this.teacherService.findAll();
--- domain/src/teacher/teacherService.ts
+++ domain/src/teacher/teacherService.ts
@@ -1,16 +1,55 @@
-import { Injectable } from '@nestjs/common';
-import { Teacher } from './Teacher';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Teacher } from './teacher';
 import { CreateTeacherDto } from './dto/createTeacher';
-
+import { v4 as uuidv4 } from 'uuid';
+import { Email } from 'shared/interfaces';
+import { UpdateTeacherDto } from './dto/updateTeacher';
 @Injectable()
 export class TeacherService {
   private readonly teachers: Teacher[] = [];
 
   create(createTeacherDto: CreateTeacherDto): Teacher {
-    return ;
+    const teacher = new Teacher(
+      createTeacherDto.name,
+      createTeacherDto.email,
+      createTeacherDto.password,
+      createTeacherDto.credentials,
+      createTeacherDto.teachingArea,
+    );
+    this.teachers.push(teacher);
+    return teacher;
   }
 
   findAll(): Teacher[] {
     return this.teachers;
   }
+
+  findById(teacherId: uuidv4 ): Teacher {
+    return this.teachers.find((teacher) => teacher.teacherId === teacherId);
+  }
+
+  findByEmailAndPassword(email: Email, password: string): Teacher {
+    return this.teachers.find((teacher) => teacher.email === email && teacher.password === password);
+  }
+  
+  update(teacherId: uuidv4, updateTeacherDto: UpdateTeacherDto): Teacher {
+    const teacherIndex = this.teachers.findIndex((teacher) => teacher.teacherId === teacherId);
+    if (teacherIndex === -1) {
+      throw new NotFoundException('Teacher not found');
+    }
+    const updatedTeacher = new Teacher(
+      updateTeacherDto.updateName ?? this.teachers[teacherIndex].name,
+      updateTeacherDto.updateEmail ?? this.teachers[teacherIndex].email,
+      updateTeacherDto.updatePassword ?? this.teachers[teacherIndex].password,
+      updateTeacherDto.updateCredentials ?? this.teachers[teacherIndex].credentials,
+      updateTeacherDto.updateTeachingAreas ?? this.teachers[teacherIndex].teachingArea,
+      this.teachers[teacherIndex].score,
+      this.teachers[teacherIndex].answeredQuestions,
+    );
+    this.teachers[teacherIndex] = updatedTeacher;
+    return updatedTeacher;
+  }
+  
+  
 }
+
