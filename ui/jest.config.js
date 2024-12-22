336eccab96ee1237bf17056cc40ff9db3d68044f
Gabriela Sanches <gabriela.sanches@gupy.com.br>
2024-12-22 15:23:38 -0300

    chore[front]: jest configuration and integration with typescript and jsx files

---
+++ ui/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
+  rootDir: './src',
+  testRegex: '.*\\.test\\.(ts|tsx)$',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  collectCoverage: true,
+  coverageDirectory: '../coverage/ui',
+  coverageReporters: ['text', 'lcov'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+};
