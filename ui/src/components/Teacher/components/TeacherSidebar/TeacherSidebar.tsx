35452756c06d4573d02312c183baac06c8e95512
Gabriela Sanches <gabriela.sanches@gupy.com.br>
2024-12-22 15:30:01 -0300

    test: TeacherSidebar rendering tested successfully

--- ui/src/components/Teacher/components/TeacherSidebar/TeacherSidebar.tsx
+++ ui/src/components/Teacher/components/TeacherSidebar/TeacherSidebar.tsx
@@ -1,19 +1,30 @@
 import React, { useState } from 'react';
-import { Flex, Stack, Text } from '@chakra-ui/react';
+import { Flex, Stack } from '@chakra-ui/react';
 import { ArrowBackIcon, ArrowLeftIcon, ArrowRightIcon, ChatIcon, LockIcon } from '@chakra-ui/icons'
 import { HomeIcon } from '@heroicons/react/24/outline';
 import { SidebarContainer, StyledSidebar, MenuWrapper, MenuItemWrapper, MenuItemText, ToggleButtonContainer, StyledToggleButton } from './TeacherSidebar.styles'
 
-const TeacherSidebar = ({ onClick = ()=>{}}) => {
-  const [collapsed, setCollapsed] = useState(false);
+interface TeacherSidebarProps {
+  onClick?: () => void;
+}
 
-  const handleToggleSidebar = () => setCollapsed(!collapsed);
-  const links = [
-    { name: 'Home', href: '/', icon: HomeIcon},
-    { name: 'Account', href: '/account', icon: LockIcon},
-    { name: 'Participation', href: '/participations', icon:ChatIcon},
-    { name: 'Logout', href: '/', icon: ArrowBackIcon}
-  ]
+interface SidebarLink {
+  name: string;
+  href: string;
+  icon: React.ComponentType<any>;
+}
+
+const TeacherSidebar: React.FC<TeacherSidebarProps> = ({ onClick }) =>  {
+  const [collapsed, setCollapsed] = useState<boolean>(false);
+
+  const handleToggleSidebar = (): void => setCollapsed((prev) => !prev);
+
+  const links: SidebarLink[] = [
+    { name: 'Home', href: '/dashboard', icon: HomeIcon },
+    { name: 'Account', href: '/account', icon: LockIcon },
+    { name: 'Participation', href: '/participations', icon: ChatIcon },
+    { name: 'Logout', href: '/', icon: ArrowBackIcon },
+  ];
 
   return (
   <React.Fragment>
