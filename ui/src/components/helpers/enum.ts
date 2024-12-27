7bcd0f0c6b8a313879a1159d8920c291eabcf097
Gabriela Sanches <gabriela.sanches@gupy.com.br>
2024-12-27 17:09:04 -0300

    feat[front]: ui enum for testing proposes

---
+++ ui/src/components/helpers/enum.ts
@@ -0,0 +1,9 @@
+export enum TeachingArea {
+  Portuguese = 'Portuguese',
+  Mathematics = 'Mathematics',
+  History = 'History',
+  Geography = 'Geography',
+  Sciences = 'Sciences',
+  Literature = 'Literature',
+  English = 'English',
+}
