792528760cd9e93a23f29e1b34234f1d28268427
Gabriela Sanches <gabriela.sanches@gupy.com.br>
2024-12-29 20:02:25 -0300

    chore: changed used port to 8081

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     static: {
       directory: path.join(__dirname, 'public'),
     },
-    port: 8080,
+    port: 8081,
     historyApiFallback: true,
     proxy: {
       '/api': {
