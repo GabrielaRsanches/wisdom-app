d53c36d25bb4e509fd949444850e621f8c9102db
Gabriela Sanches <gabriela.sanches@gupy.com.br>
2024-12-29 20:01:26 -0300

    chore: database setup with typeorm dataSource and connected with postgres

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { useContainer } from 'class-validator';
+import { AppDataSource } from './infra/dataSource';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:8080',
+    origin: 'http://localhost:8081',
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: 'Content-Type , Authorization',
   });
 
   const port = 3000;
   await app.listen(port);
   console.log(`Backend is running on: http://localhost:${port}`);
+
+  useContainer(app.select(AppModule), {
+    fallbackOnErrors: true,
+  });
 }
 
 bootstrap();
