2e4b3c1008edda348987cab5240990ea1a8c6dfb
Gabriela Sanches <gabriela.sanches@gupy.com.br>
2024-12-30 16:07:32 -0300

    feat: AppModule imports StudentModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,11 @@ import { join } from 'path';
 import { TeacherModule } from './domain/src/teacher/teacherModule';
 import { DatabaseModule } from './infra/dataSource.module';
 import { AppController } from './app.controller';
+import { StudentModule } from './domain/src/student/studentModule';
 
 @Module({
   imports: [
+    StudentModule,
     TeacherModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', '../../ui', 'dist'),
